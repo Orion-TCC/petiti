1dabb03080a868a990d9642b1acbbfa3cb8232c8
hCwai <110063239+hCaua@users.noreply.github.com>
2022-08-26 22:12:04 -0300

    Desenvolvimento do começo do site

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,41 +9,111 @@
     <link rel="stylesheet" href="styles/stylesheet.css">
     <script src="js/script.js" async></script>
 </head>
+
 <body>
     <section id="PrimeiraPartePage">
         <div id="SeguraTudo1">
             <img class="background" src="images/background.svg" alt="">
-        
+
             <nav id="navbar">
                 <div>
-                <img class="logoNavbar" src="images/logo_principal.svg" > 
+                    <img class="logoNavbar" src="images/logo_principal.svg">
                 </div>
 
                 <div>
-                <a href="#" class="navbarElement">Postagens</a>
-                <a href="#" class="navbarElement">Networking</a>
-                <a href="#" class="navbarElement">Parcerias</a>
+                    <a href="teste.php" class="navbarElement">Postagens</a>
+                    <a href="teste.php" class="navbarElement">Networking</a>
+                    <a href="teste.php" class="navbarElement">Parcerias</a>
                 </div>
 
                 <div>
-                <a href="#" class="navbarElement">Entrar</a>
-                <a href="#" class="navbarElementBotao">Cadastre-se</a>
-                </div>     
+                    <a href="teste.php" class="navbarElement">Entrar</a>
+                    <a href="teste.php" class="navbarElementBotao">Cadastre-se</a>
+                </div>
             </nav>
 
 
             <div class="descEilus">
                 <div class="descHolder">
-                <h5 class="desc"> O melhor lugar para petlovers que nem você!</h5>
-                <h5 class="desc2">Uma rede social capaz de ampliar o networking entre petlovers e empresas voltadas para animais.</h5>
+                    <span class="desc"> O melhor lugar para petlovers que nem você!</span>
+                    <span class="desc2">Uma rede social capaz de ampliar o networking entre petlovers e empresas voltadas para animais.</span>
                 </div>
 
                 <div>
-                <img src="images/ilustração - pc.svg" alt="">
+                    <img src="images/ilustração - pc.svg" alt="">
                 </div>
             </div>
         </div>
     </section>
-    
+
+    <section id="SegundaPartePage">
+        <div id="SeguraTudo2">
+
+            <div class="textinhoHolder">
+                <div class="textinho">
+                    <span class="textin">Poste fotos dos momentos mais fofos dos seus pets!</span>
+                </div>
+            </div>
+
+
+            <div class="textoEpost">
+                <div>
+                    <img class="imgPosts" src="images/posts.svg" alt="">
+                </div>
+
+                <div class="divTexto">
+                    <span class="textin2titulo">Sua mais nova rede social, apenas para pets</span>
+                    <span class="textin2"> Faça posts de seus melhores momentos com seu melhor amigo e explore seu novo vício. </span>
+                    <span class="textin2">Filtre suas publicações e pesquisas para ficarem de acordo com o seu gosto! Assim como irá facilitar que seu círculo de petmigos tenha petlovers com os mesmos interesses que você.</span>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section id="TerceiraPartePage">
+
+        <img class="faixaNetworking" src="images/conteudo - 2.svg" alt="">
+
+        <div class="faixaTextoHolder">
+            <div class="networkingTextoHolder">
+                <span class="networkingTitulo">Networking</span>
+                <span class="networkingTexto">Além de postar fotos, interaja com novas pessoas e compartilhe conhecimento.</span>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section id="TerceiraPartePage">
+
+        <div class="lojinhaHolder">
+            <div class="lojinhaTextTituloHolder">
+                <div>
+                    <span class="lojinhaTextTitulo">Além de posts, veja nossas parcerias com petshops da sua região.</span>
+                </div>
+
+                <div>
+                    <span class="lojinhaTextSubTitulo">Tudo que seu pet precisa</span>
+                </div>
+
+                <div class="lojinhaTextHolder">
+                    <span class="lojinhaText">Veja se o <span class="lojinhaTextStyle">petshop</span>, <span class="lojinhaTextStyle">casa de ração</span> ou até <span class="lojinhaTextStyle">mesmo banho e tosa</span> da sua região criou um perfil na nossa rede social e seja capaz de ver os produtos que ele oferece.</span>
+                </div>
+            </div>
+
+            <div>
+                <img class="lojinhaImg" src="images/lojinha - petshop.svg" alt="">
+            </div>
+        </div>
+
+    </section>
+
+    <section id="QuartaPartePage">
+
+
+    </section>
+
 </body>
+
 </html>
