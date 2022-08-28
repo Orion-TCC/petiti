ad4641a907727d44c8f385035305d6014c6ed3eb
hCwai <110063239+hCaua@users.noreply.github.com>
2022-08-28 00:32:31 -0300

    segunda parte da page de cadastro

---
+++ views/js/script.js
@@ -0,0 +1,9 @@
+
+let input = document.getElementById("inputTag");
+let imageName = document.getElementById("imageName")
+
+input.addEventListener("change", ()=>{
+    let inputImage = document.querySelector("input[type=file]").files[0];
+
+    imageName.innerText = inputImage.name;
+})
--- views/register/pages/register-comum/forms/formulario-foto.php
+++ views/register/pages/register-comum/forms/formulario-foto.php
@@ -11,7 +11,7 @@
     <!-- styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="../../../../css/style.css">
 
     <!-- título da pág e icone (logo) -->
     <title>Pet iti - A rede social para petlovers</title>
@@ -19,8 +19,53 @@
 
     <!--script-->
     <script src="https://kit.fontawesome.com/e08c13fee8.js" crossorigin="anonymous"></script>
+    <script src="../../../../js/script.js" async></script>
 </head>
 
 <body>
     <main class="container-content">
+        <section id="formularioFoto">
+            <div class="holderFormularioFoto">
+                <div class="formulario">
+                    <div class="imgSetaVoltarHolder">
+                    <a href="../forms/formulario-usuario.php"> <img class="imgSetaVoltar" src="../../../../img/seta - voltar.svg" alt=""></a>
+                    </div>
+
+                    <div class="tituloFormFoto">
+                        <span>Deixe seu pefil mais a sua cara!</span>
+                    </div>
+                   
+                    <form enctype="multipart/form-data" action="controllers/controller-foto.php" method="POST" class="formElementsHolder">
+                           
+                            <label class="subTituloFormFoto">
+                                Foto
+                            </label>
+                            
+                            <label class="formLabelFoto" id="inputTag">
+                               <input class="inputFormFoto" type="file" name="flFoto">
+                               Anexar foto dos meus arquivos
+                            </label>
+
+                            <label class="formTextFotoInput" id="imageName">
+                            Nenhum arquivo selecionado
+                            </label>
+
+                            <div class="EHolder">
+                                    <div class="line"></div>
+                                    <span>E</span>
+                                    <div class="line"></div>
+                            </div>
+
+                            <div class="formTextFoto">
+                                <span>Caso não tenha escolhido uma foto, você pode fazer isso depois. E, caso você tenha anexado uma foto e não tenha gostado muito, será possível alterar quando sua conta estiver feita.</span>
+                            </div>
+                          
+                            <input class="formInputFoto" type="submit" value="Continuar">
+                      </form> 
+                </div>
+            </div>
+
+        </section>
     </main>
+
+
