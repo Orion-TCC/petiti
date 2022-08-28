1f3f946773543f7fcf43933943ad0dc1c1bfc09a
hCwai <110063239+hCaua@users.noreply.github.com>
2022-08-28 00:57:02 -0300

    atualizacao segunda pag de cadastro usuario

--- views/register/pages/register-comum/forms/formulario-usuario.php
+++ views/register/pages/register-comum/forms/formulario-usuario.php
@@ -1,16 +1,25 @@
 <!DOCTYPE php>
-<html lang="en">
-
+<html lang="pt-br">
+    
 <head>
+    <!-- HTML base -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pet iti</title>
-    <script src="js/script.js" async></script>
-    <link rel="stylesheet" href="../../../../css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-</head>
+    <meta name="description" content="A Orion é uma empresa especializada em softwares para empresas de pequeno e médio porte.">
 
+    <!-- styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="../../../../css/style.css">
+
+    <!-- título da pág e icone (logo) -->
+    <title>Pet iti - A rede social para petlovers</title>
+    <link rel="icon" href="../../../../img/logo-icon.svg">
+
+    <!--script-->
+    <script src="https://kit.fontawesome.com/e08c13fee8.js" crossorigin="anonymous"></script>
+    <script src="../../../../js/script.js" async></script>
 <body>
     <main class="container-content">
         <section id="formularioUsuario">
