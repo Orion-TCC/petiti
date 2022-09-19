ea6570acbf83eb7058ba2b42d54cbf537f04af04
Kauan Oliveira <70850934+KauanMO@users.noreply.github.com>
2022-09-19 12:20:44 -0300

    Add files via upload

---
+++ database/import-db.php
@@ -0,0 +1,16 @@
+<?php
+$connection = mysqli_connect('localhost','root','','dbpetiti');
+$filename = 'dbpetiti.sql';
+$handle = fopen($filename,"r+");
+$contents = fread($handle,filesize($filename));
+$sql = explode(';',$contents);
+foreach($sql as $query){
+  $result = mysqli_query($connection,$query);
+  if($result){
+      echo '<tr><td><br></td></tr>';
+      echo '<tr><td>'.$query.' <b>SUCCESS</b></td></tr>';
+      echo '<tr><td><br></td></tr>';
+  }
+}
+fclose($handle);
+echo 'Successfully imported';
