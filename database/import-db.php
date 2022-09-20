d5ed89cc750eec65c10511f8546ca071447d2eb9
lean-dro <72211521+lean-dro@users.noreply.github.com>
2022-09-20 07:36:07 -0300

    -- import corrigido

--- database/import-db.php
+++ database/import-db.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect('localhost','root','','dbpetiti');
+$connection = mysqli_connect('localhost','root','','test');
 $filename = 'dbpetiti.sql';
 $handle = fopen($filename,"r+");
 $contents = fread($handle,filesize($filename));
