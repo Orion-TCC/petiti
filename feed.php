a3aa952451792706844a0746ed0d6012de6dd196
hCwai <110063239+hCaua@users.noreply.github.com>
2022-09-26 12:20:43 -0300

    mudanca feed

--- feed.php
+++ feed.php
@@ -25,118 +25,42 @@ include_once("sentinela.php");
     <script src="https://kit.fontawesome.com/e08c13fee8.js" crossorigin="anonymous"></script>
     <script src="assets/js/script.js"></script>
 </head>
-<body class="bodyFeed">  
+
+<body id="bodyFeed">  
+
 <main class="holderFeed">
-    
-   <section id="hud" class="hudHolder" >
-        <div class="hud">
-            <div class="elementsHud"> 
-            <img style="width:200px; padding-left: 27%; padding-top:20px;" src="assets/images/logo_principal.svg" alt="">
+    <section class="leftBarHolder">
+    <div class="leftBar">
+        <img src="./assets/images/logo_principal.svg" class="imgLogoFeed">
 
-            <div class="elementosContainer" >
-                <div class="elementos">
-            <a class="hudA" href="#">Home</a>
-            <a class="hudA" href="#">Animais perdidos</a>
-            <a class="hudA" href="#">Animais em doação</a>
-            <a class="hudA" href="#">Notificações</a>
-            <a class="hudA" href="#">Mensagens</a>
-            <a class="hudA" style="margin-top: 10px;" href="#">Produtos e serviços</a>
-            </div>
-                <div class="line"></div>
-
-                <button class="botaoCriarPost">Criar um post</button>
-                    </div>
-
-                    <!-- placeholder -->
-                    <div class="userElementos">
-
-                        <img class="imagemUser" src="<?php
-                        echo $_SESSION['foto'];?>" alt="">
-                        <div style="display: flex; flex-direction: row;">
-                            <div style="display: flex; flex-direction: column; margin-left: 10px;">
-                            
-                            <span class="textNomeUsuario"><?php
-                            echo $_SESSION['nome'];
-                            ?></span>
-
-                            <span class="textLoginUsuario"> <?php
-                            echo "@".$_SESSION['login'];
-                            ?>
-                            </span>
-
-                        
-                            
-                            </div>
-                            <div class="exit" style="display: flex; margin: 13px">
-                                <span class="textLoginUsuario">
-                               <a href="sair.php"> <img width="20px" src="assets/images/exit-login.svg" alt="" srcset=""> </a>
-                            </span>
-                            </div>
-                        </div>
-                    </div>
-            </div>
+        <div class="leftBarMenu">
+            <a href="#">Home</a>
+            <a href="#">Animais perdidos</a>
+            <a href="#">Animais em doação</a>
+            <a href="#">Notificações</a>
+            <a href="#">Mensagens</a>
+            <a href="#">Produto e serviços</a>
         </div>
-   </section>
+
+        <button class="botaoCPost"></button>
 
 
-    <section id="posts"  class="postsHolder">
-        <div class="posts">
-            <div class="elementosPosts">
-
-            <div  style=" position: absolute; z-index: 100; " >
-            <input class="barraPesquisaFeed" type="search" placeholder="Pesquisar">
-            </div>
-
-            <div class="postarFeed">
-
-                <div style="width: 369px; height: 58px; margin-top: 20px; margin-left: 240px;">
-                  <span class="textoPostarFeed"> Crie um post anexando uma foto, gif ou vídeo!</span>
-                  <span class="textoPostarFeed" style="color: #7A7A7A;"> Compartilhe seu bichinho dormindo...</span>
-                </div>
-
-                <span class="placeHolderPostarFeedBotao">Postar</span>
-            </div>
-
-            <div class="placeholderPost">
-                <img src="assets/images/reações-post.svg" style="width: 130px; margin-top: 102%; margin-left: 20px;" alt="">
-                <img src="assets/images/curtida, desc.svg" style="width: 500px; margin-left: 20px;" alt="">
-            </div>
-
-            <div class="placeholderPost">
-                <img src="assets/images/reações-post.svg" style="width: 130px; margin-top: 102%; margin-left: 20px;" alt="">
-                <img src="assets/images/curtida, desc.svg" style="width: 500px; margin-left: 20px;" alt="">
-            </div>
-
-            <div class="placeholderPost">
-                <img src="assets/images/reações-post.svg" style="width: 130px; margin-top: 102%; margin-left: 20px;" alt="">
-                <img src="assets/images/curtida, desc.svg" style="width: 500px; margin-left: 20px;" alt="">
-            </div>
-
-            </div>
-        </div>
-   </section>
-
-    <section id="function" class="functionHolder">
-            <div class="function">
-                <div class="placeholderFeedAnimalPerdido">
-                    <img src="assets/images/feed animal perdido.svg" alt="">
-                </div>
-
-                <div class="placeholderCategorias">
-                    <img src="assets/images/categorias.svg" alt="">
-                </div>
-
-                <div class="placeholderCopyright">
-                    <img src="assets/images/copyright.svg" alt="">
-                </div>
-            </div>
+    </div>
     </section>
 
 
+
+
+
+
+    <section class="postsHolder">
+
+    </section>
+    
+        <section class="rightBarHolder">
+
+        </section>
 </main>
 
-
 </body>
-
-
 </html>
