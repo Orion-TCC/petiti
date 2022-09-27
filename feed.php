a74ae5b59cb7d423188cf73f10c5b3d7dd953e72
hCwai <110063239+hCaua@users.noreply.github.com>
2022-09-27 03:43:14 -0300

    feed em producao

--- feed.php
+++ feed.php
@@ -16,6 +16,7 @@ include_once("sentinela.php");
     <!-- styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <link rel="stylesheet" href="assets/styles/stylesheet.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
     <!-- título da pág e icone (logo) -->
     <title>Pet iti - A rede social para petlovers</title>
@@ -31,18 +32,46 @@ include_once("sentinela.php");
 <main class="holderFeed">
     <section class="leftBarHolder">
     <div class="leftBar">
-        <img src="./assets/images/logo_principal.svg" class="imgLogoFeed">
+        
 
         <div class="leftBarMenu">
+
+        <div class="innerLeftBarMenu">
+        
+        <div style="display: flex; flex-direction: column;">
+        <img src="./assets/images/logo_principal.svg" class="imgLogoFeed">
             <a href="#">Home</a>
             <a href="#">Animais perdidos</a>
             <a href="#">Animais em doação</a>
             <a href="#">Notificações</a>
             <a href="#">Mensagens</a>
             <a href="#">Produto e serviços</a>
+
+            <hr class="line">
+
+           <button class="botaoCPost" > Criar um post</button>
+
         </div>
 
-        <button class="botaoCPost"></button>
+           <div class="userElementos">
+
+            <img class="imagemUser" src="<?php
+            echo $_SESSION['foto'];?>" alt="">
+
+            <div style="display: flex; flex-direction: column; margin-left: 10px;">
+
+            <span class="textNomeUsuario"><?php
+            echo $_SESSION['nome'];
+            ?></span>
+
+            <span class="textTagUsuario"> <?php
+            echo "@".$_SESSION['login'];
+            ?>
+            </div>
+        </div>
+        </div>
+        
+       
 
 
     </div>
