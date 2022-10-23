c5ce57c9a1f35fd2c3860caa3490102315f15a8d
hCwai <110063239+hCaua@users.noreply.github.com>
2022-10-23 17:00:57 -0300

    func n sei

--- assets/js/funcs.js
+++ assets/js/funcs.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 $(document).ready(function () {
   var id = "";
   $(".curtir").on("click",function () {
@@ -35,3 +36,76 @@ $(document).ready(function () {
     });
   });
 });
+=======
+$(document).ready(function () {
+  var id = "";
+  $(".curtir").on("click",function () {
+    id = $(this).val();
+    $.ajax({
+      type: "POST",
+      url: "/petiti/api/curtir",
+      data: {"id":id },
+      success: function (data) {
+        console.log("post de id "+id+" foi curtido");
+        $("#itimaliasPost"+id).text(data+" itimalias");
+      }
+    });
+  });
+  
+  $(".comentar").on("click",function () {
+    id = $(this).val();
+    console.log(id);
+    var texto = $("#txtComentar"+id).val();
+    console.log(texto);
+    $.ajax({
+      type: "POST",
+      url: "/petiti/api/comentar",
+      data: {"id":id, "texto": texto},
+      success: function (data) {
+        console.log();
+        console.log(data[0].nomeUsuario+" "+data[0].textoComentario);
+      }
+    });
+  });
+});
+
+
+
+
+function showHideElement() {
+  var x = document.getElementById("categoriasHolder");
+
+  var expandMore = document.getElementById("expandMoreIcon");
+  var expandLess = document.getElementById("expandLessIcon");
+  
+  var botaoShowHide = document.getElementById("botaoShowHide");
+
+  var categoriasHolder = document.getElementById("categoriasHolder");
+  
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+    expandMore.style.display = "none";
+    expandLess.style.display = "block";
+    
+    botaoShowHide.style.borderBottom = "none";
+    botaoShowHide.style.paddingBottom = "1px";
+    categoriasHolder.style.borderBottom = "1px solid gray";
+    
+    
+  } else {
+    x.style.display = "none";
+    expandMore.style.display = "block";
+    expandLess.style.display = "none";
+    
+    botaoShowHide.style.borderBottom = "1px solid gray";
+    botaoShowHide.style.paddingBottom = "none";
+
+    categoriasHolder.style.borderBottom = "none"
+  }
+}
+
+
+
+
+
+>>>>>>> Stashed changes
