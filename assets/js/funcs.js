6af57982f618ad8b92ee7fc2ff2f1e85ca3843bd
lean-dro <72211521+lean-dro@users.noreply.github.com>
2022-10-23 22:09:28 -0300

    correção

--- assets/js/funcs.js
+++ assets/js/funcs.js
@@ -35,37 +35,6 @@ $(document).ready(function () {
     });
   });
 });
-$(document).ready(function () {
-  var id = "";
-  $(".curtir").on("click",function () {
-    id = $(this).val();
-    $.ajax({
-      type: "POST",
-      url: "/petiti/api/curtir",
-      data: {"id":id },
-      success: function (data) {
-        console.log("post de id "+id+" foi curtido");
-        $("#itimaliasPost"+id).text(data+" itimalias");
-      }
-    });
-  });
-  
-  $(".comentar").on("click",function () {
-    id = $(this).val();
-    console.log(id);
-    var texto = $("#txtComentar"+id).val();
-    console.log(texto);
-    $.ajax({
-      type: "POST",
-      url: "/petiti/api/comentar",
-      data: {"id":id, "texto": texto},
-      success: function (data) {
-        console.log();
-        console.log(data[0].nomeUsuario+" "+data[0].textoComentario);
-      }
-    });
-  });
-});
 
 
 
