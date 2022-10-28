e3b008b9f988a1a91c4ed92ba8a149a3c6ad93b0
lean-dro <72211521+lean-dro@users.noreply.github.com>
2022-10-28 15:23:21 -0300

    Erro 404

--- views/erroGeral.php
+++ views/erroGeral.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="icon" href="/petiti/views/assets/img/logo-icon.svg">
+    <link rel="stylesheet" href="/petiti/views/assets/css/style.css" />
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Página Inexistente - peti iti</title>
 </head>
+
 <body>
-    <h1>Essa página não existe, beleza?</h1>
-    <a href="/petiti/">Voltar pra home</a>
+    <div class="fundo404"></div>
 </body>
+
 </html>
