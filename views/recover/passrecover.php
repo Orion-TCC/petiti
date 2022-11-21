915a12060ed4f5faacbdb55d81f19d7d1081de5a
hCwai <110063239+hCaua@users.noreply.github.com>
2022-11-20 23:28:45 -0300

    recuperação de senha

--- views/recover/passrecover.php
+++ views/recover/passrecover.php
@@ -44,6 +44,57 @@
             
         </form>
 
+        <div class="holderFormularioUsuario">
+                <div class="formulario">
+                    
+                    <div class="tituloFormHolder">
+                        <span>
+                            Vamos começar!
+                        </span>
+                    </div>
+                    <div class="subTituloFormHolderUsuario">
+                        <span>
+                            Insira seus dados de acesso abaixo:
+                        </span>
+                    </div>
+                    <div class="formularioHolder ">
+                        <form class="formElementsHolder" action="api/usuario/add" method="post">
+                            <label class="formText">Email</label>
+                            <input class="formInput" autocomplete="off" placeholder="Insira seu email" type="email" name="txtEmailUsuario" id="txtEmailUsuario" required autofocus>
+                            <p id="avisoEmail"></p>
+
+
+                            <label class="formText">Nome de usuário</label>
+                            <input class="formInput" autocomplete="off" placeholder="Insira seu nome de usuario" type=" text" name="txtLoginUsuario" id="txtLoginUsuario" required minlength="4">
+
+                            <p class="avisoNomeUsuarioValidacao"></p>
+                            <p class="avisoNomeUsuarioQtd"></p>
+
+                            <label class="formText">Senha</label>
+                            <div class="formInput">
+                                <input  placeholder="Insira sua melhor senha" autocomplete="off" type="password" name="txtPw" id="txtPw" required minlength="6">
+                               
+                                <i id="revealPassword" onclick="hidePassword()" class="uil uil-eye"></i>
+                                <i id="hidePassword" onclick="showPassword()" class="uil uil-eye-slash"></i>
+                            </div>
+                            <p id="senhaAvisoTamanho"></p>
+
+
+                            <label class="formText">Confirme sua senha</label>
+                            <input class="formInput" autocomplete="off" placeholder="Confirme a senha" type="password" name="txtPwConfirm" id="txtPwConfirm" required minlength="6">
+                            <p id="senhaAvisoVerificacao"></p>
+
+
+                            <button id="submitUsuario" class="formSubmit" type="submit">Continuar</button>
+
+                                <span class="textoErrado" style="align-self: center;"> <?php echo @$_COOKIE["erro-cadastro"];?></span>
+                        </form>
+                    </div>
+
+
+
+                </div>
+            </div>
     </div>
 
 </body>
