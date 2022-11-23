d371af32f8bad2741936d54168c71005f38539f0
Kauan Oliveira <70850934+KauanMO@users.noreply.github.com>
2022-11-23 08:21:16 -0300

    Add files via upload

--- views/perfil/perfilUsuario.php
+++ views/perfil/perfilUsuario.php
@@ -199,7 +199,7 @@ $usuario->login($_SESSION['login'], $_SESSION['senha']);
                 <!-- SIDEBAR LADO ESQUERDO -->
 
                 <div class="sidebar">
-                    <a href="#" class="menu-item">
+                    <a href="feed" class="menu-item">
                         <span><i class="uil uil-house-user"></i> </span>
                         <h3>Home</h3>
                     </a>
