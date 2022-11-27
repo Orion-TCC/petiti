97169b1c4feb7c58e70888db8ba75d2a9e84461e
Kauan <kauaanmatheus@gmail.com>
2022-11-26 21:24:34 -0300

    testa agora caua

--- api/index.php
+++ api/index.php
@@ -688,8 +688,7 @@ $app->post('/pet/add', function (Request $request, Response $response, array $ar
         );
         if ($arrayData[$slDiaMesAno] == "dias" && $idade >= 31) {
             if ($idade >= 365) {
-                $idade = intdiv($idade, 30);
-                $idade = intdiv($idade, 12);
+                $idade = intdiv($idade, 365);
                 $arrayData[$slDiaMesAno] = "anos";
             } else {
                 $idade = intdiv($idade, 30);
