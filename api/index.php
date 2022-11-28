a406d2fd00c6439c8cc941beb99cb0448b6684c1
Kauan Oliveira <70850934+KauanMO@users.noreply.github.com>
2022-11-28 09:28:29 -0300

    att denuncias

--- api/index.php
+++ api/index.php
@@ -286,6 +286,7 @@ $app->get('/bloquear-tutor-denunciado/{tipoDenuncia}/{idDenunciado}/{idDen}', fu
     $usuario->updateStatus($usuario);
     $denunciaPublicacao = new DenunciaPublicacao();
     $denunciaUsuario = new DenunciaUsuario();
+    $denunciaComentario = new DenunciaComentario();
     $cookie = new Cookies;
 
     $cookie->criarCookie(
@@ -324,6 +325,11 @@ $app->get('/bloquear-tutor-denunciado/{tipoDenuncia}/{idDenunciado}/{idDen}', fu
         $decisao = "Usuario denunciador bloqueado";
         $denunciaUsuario->updateDecisao($args['idDen'], $decisao);
     } else if ($args['tipoDenuncia'] == "comentario") {
+        $denunciaComentario->setIdDenunciaComentario($args['idDen']);
+        $denunciaComentario->setStatusDenunciaComentario(2);
+        $denunciaComentario->updateStatus($denunciaComentario);
+        $decisao = "Usuario bloqueado";
+        $denunciaComentario->updateDecisao($args['idDen'], $decisao);
     }
 
 
@@ -745,7 +751,7 @@ $app->post('/add-pet-config', function (Request $request, Response $response, ar
     $cookie = new Cookies();
     $fotoPet = new FotoPet();
 
-    
+
     $arrayEspecies = array(
         1 => "Cachorro",
         2 => "Gato",
@@ -811,11 +817,11 @@ $app->post('/add-pet-config', function (Request $request, Response $response, ar
     $pet->setIdPet($id);
     $fotoPet->setPet($pet);
 
-    if($_POST['baseFoto'] == "padrao"){
+    if ($_POST['baseFoto'] == "padrao") {
         $fotoPet->setNomeFotoPet("padrao.png");
         $fotoPet->setCaminhoFotoPet("private-user/fotos-pet/padrao.png");
         $fotoPet->cadastrar($fotoPet);
-    }else{
+    } else {
         $image = $_POST['baseFoto'];
         $caminhoSalvar = "/xampp/htdocs/petiti/private-user/fotos-pet/";
         $nomeArquivo = time() . ".png";
@@ -960,7 +966,7 @@ $app->get('/publicacoes/personalizadas/{id}', function (Request $request, Respon
     return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
 });
 
-$app->get('/produtos/{id}', function (Request $request, Response $response, array $args){
+$app->get('/produtos/{id}', function (Request $request, Response $response, array $args) {
     $produto = new Produto();
     $id = $args['id'];
 
@@ -969,7 +975,7 @@ $app->get('/produtos/{id}', function (Request $request, Response $response, arra
     return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
 });
 
-$app->get('/servicos/{id}', function (Request $request, Response $response, array $args){
+$app->get('/servicos/{id}', function (Request $request, Response $response, array $args) {
     $servico = new Servico();
     $id = $args['id'];
 
@@ -1295,7 +1301,7 @@ $app->post(
         } else {
             $publicacao->setImpulsoPub(0);
         }
-        
+
         $id = $publicacao->cadastrar($publicacao);
 
         $caminhoSalvar = "/xampp/htdocs/petiti/private-user/fotos-publicacao/";
@@ -1663,7 +1669,7 @@ $app->post('/config-conta', function (Request $request, Response $response, arra
     }
 
     if (isset($_POST['txtEmail'])) {
-        if($_POST['txtEmail']!=$_SESSION['email']){
+        if ($_POST['txtEmail'] != $_SESSION['email']) {
             $usuario->setEmailUsuario($_POST['txtEmail']);
             $emailVerificacao = $usuario->verificarEmail($_POST['txtEmail']);
             if ($emailVerificacao == true) {
@@ -1786,6 +1792,33 @@ $app->get('/excluir-denuncia/{tipoDenuncia}/{id}', function (Request $request, R
 
         header("location: /petiti/denuncias-dashboard");
     } else if ($args['tipoDenuncia'] == "comentario") {
+        $denunciaComentario = new denunciaComentario();
+        $denunciaComentario->setIdDenunciaComentario($args['id']);
+        $denunciaComentario->setStatusDenunciaComentario(2);
+
+        $denunciaComentario->updateStatus($denunciaComentario);
+
+        $decisao = "Denúncia Excluída";
+
+        $denunciaComentario->updateDecisao($args['id'], $decisao);
+
+        $cookie->criarCookie(
+            "denunciaApagada",
+            "<div class='popup'></div>
+            <div class='toast'>
+                <div class='toast-content'>
+                    <div class='message'>
+                        <span class='texto-1'>Denúncia apagada com sucesso</span>
+                    </div>
+                </div>
+                 <i class='fa-sharp fa-solid fa-xmark' id='close' onclick='closePopup()'></i>
+                <div class='progressbar'></div>
+            </div>
+  ",
+            1
+        );
+
+        header("location: /petiti/denuncias-dashboard");
     }
 });
 
@@ -1870,11 +1903,11 @@ $app->post('/pesquisa-seguindo', function (Request $request, Response $response,
         $seSegue = $usuarioSeguidor->verificarSeguidor($idSession, $idUsuario);
         $tipoUsuario = $arraySeguindo[$r]['tipoUsuario'];
 
-            if($tipoUsuario == "Tutor"){
-                $iconTipoUsuario = "user";
-            }else{
-                $iconTipoUsuario = "building";
-            }
+        if ($tipoUsuario == "Tutor") {
+            $iconTipoUsuario = "user";
+        } else {
+            $iconTipoUsuario = "building";
+        }
 
         if ($idUsuario == $idSession) {
             $resultSeSegue = "";
@@ -1929,7 +1962,7 @@ $app->post('/pesquisa-seguindo', function (Request $request, Response $response,
     echo ("</div>");
 });
 
-$app->post('/pesquisa-seguidores-pet', function (Request $request, Response $response, array $args){
+$app->post('/pesquisa-seguidores-pet', function (Request $request, Response $response, array $args) {
     error_reporting(0);
 
     @session_start();
@@ -1951,26 +1984,26 @@ $app->post('/pesquisa-seguidores-pet', function (Request $request, Response $res
          
          <div class='segueHolder'>");
 
-         for ($t = 0; $t < $countSeguidores; $t++) {
-            $idSeguidor = $arraySeguidores[$t]['idSeguidor'];
-            $caminhoFoto = $fotoUsuario->exibirFotoUsuario($idSeguidor);
-            $loginUsuario = $arraySeguidores[$t]['loginUsuario'];
-            $nomeUsuario = $arraySeguidores[$t]['nomeUsuario'];
-            $tipoUsuario = $arraySeguidores[$t]['tipoUsuario'];
+    for ($t = 0; $t < $countSeguidores; $t++) {
+        $idSeguidor = $arraySeguidores[$t]['idSeguidor'];
+        $caminhoFoto = $fotoUsuario->exibirFotoUsuario($idSeguidor);
+        $loginUsuario = $arraySeguidores[$t]['loginUsuario'];
+        $nomeUsuario = $arraySeguidores[$t]['nomeUsuario'];
+        $tipoUsuario = $arraySeguidores[$t]['tipoUsuario'];
 
-            if($idTutorPet == $idSession){
-                $srcFotoSeguidor = "./";
-            }else{
-                $srcFotoSeguidor = "../";
-            }
+        if ($idTutorPet == $idSession) {
+            $srcFotoSeguidor = "./";
+        } else {
+            $srcFotoSeguidor = "../";
+        }
 
-            if($tipoUsuario == "Tutor"){
-                $iconTipoUsuario = "user";
-            }else{
-                $iconTipoUsuario = "building";
-            }
-    
-            echo ("
+        if ($tipoUsuario == "Tutor") {
+            $iconTipoUsuario = "user";
+        } else {
+            $iconTipoUsuario = "building";
+        }
+
+        echo ("
             
                 <a href='/petiti/$loginUsuario' target='_blank' '>
                     <div class='seguidores-row usuarioSegue'>
@@ -1998,8 +2031,8 @@ $app->post('/pesquisa-seguidores-pet', function (Request $request, Response $res
                 </a>
             
             ");
-        } // botão seguir pra quem for mexer nisso >>> <button class='btn btn-primary'>Seguir</button>
-        echo ("</div>");
+    } // botão seguir pra quem for mexer nisso >>> <button class='btn btn-primary'>Seguir</button>
+    echo ("</div>");
 });
 
 $app->post('/pesquisa-seguidores', function (Request $request, Response $response, array $args) {
@@ -2012,7 +2045,7 @@ $app->post('/pesquisa-seguidores', function (Request $request, Response $respons
     $arraySeguidores = $usuarioSeguidor->pesquisaSeguidores($_POST['idUsuario']);
     $countSeguidores = count($arraySeguidores);
 
-    
+
     echo ("<div class='segueTitulo'>
             <h2>Seguidores</h2>
          </div>
@@ -2027,12 +2060,12 @@ $app->post('/pesquisa-seguidores', function (Request $request, Response $respons
         $seSegue = $usuarioSeguidor->verificarSeguidor($idSeguidor, $idSession);
         $tipoUsuario = $arraySeguidores[$t]['tipoUsuario'];
 
-            if($tipoUsuario == "Tutor"){
-                $iconTipoUsuario = "user";
-            }else{
-                $iconTipoUsuario = "building";
-            }
-            
+        if ($tipoUsuario == "Tutor") {
+            $iconTipoUsuario = "user";
+        } else {
+            $iconTipoUsuario = "building";
+        }
+
         if ($idSeguidor == $idSession) {
             $resultSeSegue = "";
             $seguirOuSeguindo = "";
@@ -2260,18 +2293,18 @@ $app->post('/pesquisar', function (Request $request, Response $response, array $
     }
 });
 
-$app->post('/cadastrar-produto-servico', function (Request $request, Response $response, array $args){
+$app->post('/cadastrar-produto-servico', function (Request $request, Response $response, array $args) {
     $produto = new Produto();
     $servico = new Servico();
     $usuario = new Usuario();
     $fotoServico = new FotoServico();
     $fotoProduto = new FotoProduto();
 
-   
+
 
     @session_start();
 
-    if($_POST['tipoCad'] == "produto"){
+    if ($_POST['tipoCad'] == "produto") {
         $textoProduto = $_POST['titulo'];
         $descProduto = $_POST['descricao'];
         $valorProduto = $_POST['valor'];
@@ -2298,11 +2331,11 @@ $app->post('/cadastrar-produto-servico', function (Request $request, Response $r
         $fotoProduto->setNomeFotoProduto($nomeArquivo);
         $fotoProduto->cadastrar($fotoProduto);
 
-       
+
         file_put_contents($arquivoCompleto, file_get_contents($image));
 
         header('location: /petiti/decidir-perfil');
-    }else{
+    } else {
         $textoServico = $_POST['titulo'];
         $descServico = $_POST['descricao'];
         $valorServico = $_POST['valor'];
@@ -2314,7 +2347,7 @@ $app->post('/cadastrar-produto-servico', function (Request $request, Response $r
         $usuario->setIdUsuario($_SESSION['id']);
         $servico->setUsuario($usuario);
         $id = $servico->cadastrar($servico);
-        
+
         $servico->setIdServico($id);
         $fotoServico->setServico($servico);
 
