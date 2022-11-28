b8790719588a5001bf3cef21410d433ac7d9b76c
Marina Oliveira <68068215+m-arina@users.noreply.github.com>
2022-11-28 09:20:48 -0300

    Correção página de pet alheio

---
+++ views/feed/pet.php
@@ -0,0 +1,462 @@
+<?php
+@session_start();
+include_once("../../sentinela.php");
+require_once('../../api/classes/Usuario.php');
+require_once('../../api/classes/Pet.php');
+require_once('../../api/classes/PetSeguidor.php');
+
+@session_start();
+date_default_timezone_set('America/Sao_Paulo');
+
+$pet = new Pet();
+$loginPet = $_GET['pet'];
+$idPetEscolhido = $pet->buscaPets($loginPet);
+
+$_SESSION['idPetPagina'] = $idPetEscolhido;
+
+
+$urlPerfil = "http://localhost/petiti/api/pet/$idPetEscolhido";
+$jsonPerfil = file_get_contents($urlPerfil);
+
+$dadosPerfil = json_decode($jsonPerfil);
+$idade = $dadosPerfil[0]->idadePet;
+$nome = $dadosPerfil[0]->nomePet;
+$raca = $dadosPerfil[0]->racaPet;
+$usuarioPet = $dadosPerfil[0]->usuarioPet;
+$especie = $dadosPerfil[0]->especiePet;
+$foto = $dadosPerfil[0]->caminhoFotoPet;
+$idUsuarioTutor = $dadosPerfil[0]->idUsuario;
+$tutor = $dadosPerfil[0]->loginUsuario;
+$id = $_SESSION['id'];
+if ($idUsuarioTutor == $id) {
+    header("location: /petiti/api/escolher-pet/$idPetEscolhido");
+}
+
+$url = "http://localhost/petiti/api/publicacoes/usuario/" . $idUsuarioTutor;
+
+$json = file_get_contents($url);
+$dados = (array)json_decode($json, true);
+$contagem = count($dados['publicacoes']);
+
+$urlPets = "http://localhost/petiti/api/usuario/$id/pets";
+
+$jsonPets = file_get_contents($urlPets);
+
+$dadosPets = (array) json_decode($jsonPets, true);
+
+$contagemPets = count($dadosPets['pets']);
+
+$conexao = Conexao::conexao();
+
+
+$query = "SELECT COUNT(idPetSeguidor) as qtdSeguidores FROM tbpetseguidor WHERE idPetSeguido = $idPetEscolhido";
+$resultado = $conexao->query($query);
+$lista = $resultado->fetchAll();
+$qtdSeguidores = $lista[0]['qtdSeguidores'];
+
+$petSeguidor = new PetSeguidor();
+$verificarSeguidor = $petSeguidor->verificarSeguidor($idPetEscolhido, $id);
+if ($verificarSeguidor['boolean'] == true) {
+    $jsSeguidor = "true";
+} else {
+    $jsSeguidor = "false";
+}
+
+?>
+
+
+
+
+<!DOCTYPE php>
+<html lang="pt-br">
+
+<head>
+    <!-- HTML base -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A Orion é uma empresa especializada em softwares para empresas de pequeno e médio porte.">
+
+    <!-- styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="/petiti/assets/css/feed-style.css">
+    <link rel="stylesheet" href="/petiti/assets/css/usuario-style.css">
+    <link rel="stylesheet" href="/petiti/assets/libs/croppie/croppie.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
+
+    <!--- iconscout icon --->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
+
+    <!-- título da pág e icone (logo) -->
+
+    <title>Pet iti - meu perfil </title>
+
+
+    <link rel="icon" href="/petiti/assets/images/logo-icon.svg">
+
+    <!--script-->
+
+    <script src="https://kit.fontawesome.com/e08c13fee8.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/exif-js"></script>
+    <script src="/petiti/assets/libs/croppie/croppie.js"></script>
+    <script src="/petiti/views/assets/js/script-jquery-foto.js"></script>
+    <script src="/petiti/assets/js/script.js"></script>
+    <script src="/petiti/views/assets/js/funcs.js"></script>
+</head>
+
+<body class="feed perfilUsuario">
+
+    <nav class="feed">
+        <div class="container">
+            <div class="popupOptions" id="popup">
+
+                <div class="flex-col">
+
+                    <div class="flex-row">
+                        <div class="fotoDePerfil">
+                            <img src="../<?php echo $_SESSION['foto']; ?>" alt="">
+                        </div>
+                        <h3><a href="tutor-perfil"><?php echo $_SESSION['nome']; ?></a></h3>
+                    </div>
+
+                    <?php for ($p = 0; $p < $contagemPets; $p++) { ?>
+                        <div class="flex-row petUser">
+
+                            <a class="hrefNomePet" href="/petiti/api/escolher-pet/<?php echo $dadosPets['pets'][$p]['idPet'] ?>">
+                                <div class="fotoDePerfil">
+                                    <img src="../<?php echo $dadosPets['pets'][$p]['caminhoFotoPet'] ?>" alt="">
+                                    <!--Foto do pet  -->
+                                </div>
+
+                                <h3><?php echo $dadosPets['pets'][$p]['nomePet'] ?></h3>
+                            </a>
+                        </div>
+                    <?php    } ?>
+                </div>
+
+
+                <div class="flex-col borderTop row-gap opcoesPopUp">
+
+                    <h3 style="width: 15rem;">Adicionar conta existente</h3>
+
+                    <h3>Gerenciar contas</h3>
+
+                    <h3> <a href="/petiti/opcoes">Configurações</a></h3>
+
+                    <h3><a href="/petiti/sair" class="botaoLogout"> <i class="uil uil-sign-out-alt"></i>Sair</a></h3>
+
+                </div>
+
+            </div>
+
+            <h2 class="logo">
+                <img src="../assets/images/logo_principal.svg">
+            </h2>
+            <div class="caixa-de-busca">
+                <i class="uil uil-search"></i>
+                <input class="inputSearch" autocomplete="off" id="inputSearch" type="search" placeholder="Pesquisar">
+                <div id="resultadoPesquisa" class="resultadoPesquisa">
+                </div>
+            </div>
+
+
+
+            <div class="opcoes" id="opcoes">
+
+                <div id="labelAO"><i id="settings-icon" class="uil uil-setting"></i></div>
+
+                <div class="fotoDePerfil" id="fotoDePerfil">
+                    <img src="../<?php echo $_SESSION['foto']; ?>" alt="" id="fotoDePerfilOpcoes">
+                </div>
+
+            </div>
+        </div>
+    </nav>
+
+    <main class="feed">
+        <div class="container">
+
+            <!-- LADO ESQUERDO -->
+            <div class="ladoEsquerdo">
+
+                <a href="/petiti/decidir-perfil" class="perfil">
+                    <div class="fotoDePerfil">
+                        <img src="../<?php echo $_SESSION['foto']; ?>" alt="">
+                    </div>
+                    <div class="handle">
+                        <h4><?php echo $_SESSION['nome']; ?></h4>
+                        <p class="text-muted">
+                            <?php echo "@" . $_SESSION['login']; ?>
+                        </p>
+                    </div>
+                </a>
+                <!-- SIDEBAR LADO ESQUERDO -->
+
+                <div class="sidebar">
+                    <a href="/petiti/feed" class="menu-item ativo">
+                        <span><i class="uil uil-house-user"></i> </span>
+                        <h3>Home</h3>
+                    </a>
+
+                    <a href="/petiti/animaisPerdidos" class="menu-item">
+                        <span><i class="uil uil-heart-break"></i></span>
+                        <h3>Animais Perdidos</h3>
+                    </a>
+
+                    <a href="/petiti/animaisEmAdocao" class="menu-item">
+                        <span><i class="uil uil-archive"></i> </span>
+                        <h3>Animais para Adoção</h3>
+                    </a>
+
+
+                    <a href="/petiti/notificacoes" class="menu-item">
+                        <span class="mostrarNotificacoes" style="position: relative;">
+                            <i class="uil uil-bell notificacao"></i>
+
+                        </span>
+                        <h3>Notificações</h3>
+                    </a>
+
+                    <a href="#" class="menu-item">
+                        <span><i class="uil uil-envelope"></i> </span>
+                        <h3>Mensagens</h3>
+                    </a>
+
+                    <a href="#" class="menu-item">
+                        <span><i class="uil uil-shopping-bag"></i> </span>
+                        <h3>Produtos e Serviços</h3>
+                    </a>
+
+                    <a href="/petiti/paraVoce" class="menu-item">
+                        <span><i class="uil uil-coffee"></i> </span>
+                        <h3>Para Você</h3>
+                    </a>
+                </div>
+
+
+
+
+
+                <!-- Botao de criar post -->
+                <button class="btn btn-primary">
+                    <p>
+                        <a href="#modal-foto-post" rel="modal:open">Criar um Post</a>
+                    </p>
+                </button>
+
+            </div>
+            <!-- FIM DO LADO ESQUERDO -->
+
+            <div class="Meio">
+                <div class="userArea">
+
+                    <div class="userHandle">
+
+                        <div class="userCima">
+                            <div class="fotoDePerfil">
+                                <img src="../<?php echo $foto ?>" alt=""> <!--  foto do pet  -->
+                            </div>
+
+                            <div class="userInfo">
+
+                                <div class="infoHolder topo">
+                                    <input id="jsSeguidor" value="<?php echo $jsSeguidor ?>" type="hidden">
+
+                                    <div class="flex-row" style="gap: 2rem;">
+                                        <h2><?php echo $usuarioPet; ?></h2>
+                                        <?php if ($verificarSeguidor['boolean'] == true) { ?>
+                                            <button value="<?php echo $idPetEscolhido ?>" class="seguirPet btn btn-primary">Seguir</button>
+                                        <?php } else { ?>
+                                            <button value="<?php echo $idPetEscolhido ?>" class="seguirPet btn btn-secundary">Seguindo</button>
+                                        <?php } ?>
+                                    </div>
+                                </div>
+
+                                <div class="infoHolder meio">
+                                    <h3> <?php echo $contagem ?> <span class="text-muted"> postagens </span></h3>
+                                    <h3 class="hSeguidoresPet" id="<?php echo $idPetEscolhido; ?>"><a href="#modal-seguidores" rel="modal:open" style="color: black;"> <span id="seguidores"> <?php echo $qtdSeguidores ?> </span> <span class="text-muted">seguidores</span></a></h3>
+                                </div>
+
+                                <div class="infoHolder baixo">
+                                    <div style="width: 15rem; display: flex; align-items: center;">
+                                        <i class="icon enciclopedia"></i>
+                                        <h4><?php echo $raca ?></h4>
+                                    </div>
+
+                                    <div style="width: 15rem; display: flex; align-items: center;">
+                                        <i class="icon boloDeAniversario"></i>
+                                        <h4><?php echo $idade ?></h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="userBaixo">
+
+                            <div class="subUserBaixo">
+                                <div style="width: fit-content; max-width: 25rem; display: flex; align-items: center;">
+                                    <h2><?php echo $nome ?></h2>
+                                </div>
+
+                                <h4 class="text-muted">(Meu dono(a) é <a href="/petiti/<?php echo $tutor ?>">@<?php echo $tutor ?></a>)</h4>
+                            </div>
+
+
+                        </div>
+                    </div>
+                    <!-- fim da parte de informacao do usuario -->
+
+                    <div class="tabs">
+
+                        <div class="userTabs ">
+                            <button class="userTabOption userTabOption--ativo" data-for-tab="1">Postagens</button>
+                            <button class="userTabOption" data-for-tab="2">Marcações</button>
+                        </div>
+                        <!-- fim das tabs de navegacao de usuario -->
+
+                        <div class="tabs_content postagens tabAtiva" data-tab="1">
+                            <?php
+
+                            if ($contagem < 1) { ?>
+                                <div class="aviso">
+                                    <h3>Não há postagens ainda. Faça uma clicando no botão “Criar um post”!</h3>
+                                </div>
+
+                                <?php } else {
+
+                                for ($i = 0; $i < $contagem; $i++) {
+                                    $foto = $dados['publicacoes'][$i]['caminhoFoto'];
+                                ?>
+                                    <div class="previewPostImage">
+                                        <img src="../<?php echo $foto ?>" alt="">
+                                    </div>
+
+                            <?php }
+                            } ?>
+
+                        </div>
+
+                        <div class="tabs_content marcacoes" data-tab="2">
+
+                            <div class="aviso">
+                                <h3>Parece que ninguém te marcou em um post ainda...</h3>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- fim do meio -->
+
+        </div>
+        <!-- MODALS -->
+
+        <section>
+            <div id="modal-post" class="modal post">
+                <div style="display: flex; width: 100%; height: 100%;">
+
+                    <div id="preview-crop-image">
+                        <img src="#" alt="">
+                    </div>
+
+
+                    <div class="rightSidePost">
+
+                        <div class="userElementosHolder">
+                            <div class="userElementos">
+                                <img src="#" alt="" class="fotoDePerfil">
+                                <div>
+                                    <span class="textNomeUsuario">nome</span>
+                                    <h5 class="text-muted">data</h5>
+                                </div>
+                            </div>
+
+                            <div class="editButton">
+                                <div class="menuPostHover"></div>
+                                <i class="uil uil-ellipsis-v"></i>
+                            </div>
+                        </div>
+
+                        <div class="comentariosHolder">
+
+                            <div class="comentarioHolder">
+
+                                <div class="fotoDePerfil">
+                                    <img src="#" alt="">
+                                </div>
+
+                                <div class="comentarioInfos">
+
+                                    <div class="info">
+                                        <div style="  word-break: break-all;">
+                                            <h4 class="text-muted"><span style="color: black;">Nome</span> comentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentariocomentario</h4>
+                                        </div>
+                                    </div>
+
+                                    <div class="info">
+                                        <h5 class="text-muted">tempo</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="botoesInteracao">
+
+                            <input class="curtir" value="<?php echo $id ?>" type="checkbox">
+
+                            <button class="comentar"></button>
+
+                            <button class="mensagem"></button>
+
+                        </div>
+
+                        <div class="curtidas">
+                            <h4>0 itimalias</h4>
+
+                        </div>
+
+                        <div class="commentArea">
+
+                            <i class="uil uil-heart"></i>
+
+                            <textarea oninput="auto_grow(this)" cols="30" rows="10" placeholder="Adicione um comentário!" maxlength="200" name="txtComentar<?php echo $id ?>" id="txtComentar<?php echo $id ?>"></textarea>
+
+                            <button value="<?php echo $id ?>" class="comentar" value="">
+                                <i class="uil uil-message"></i>
+                            </button>
+
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            </div>
+        </section>
+
+        <section>
+            <div id="modal-seguidores" class="modal">
+
+            </div>
+        </section>
+
+
+    </main>
+
+</body>
+
+</html>
+
+
+
+
+<!-- <button class="seguir" value="1">seguir</button> -->
